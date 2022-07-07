ca66b0dd61d2f4f558c43b9c5bb701e2a42639fe
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2022-07-07 18:55:23 +0530

    Test version with required params

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,23 @@
-FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/latch-base:9a7d-main
+FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/latch-base:02ab-main
 
-RUN apt-get install -y curl unzip
+# Install dependencies
 
-# Its easy to build binaries from source that you can later reference as
-# subprocesses within your workflow.
-RUN curl -L https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.4.4/bowtie2-2.4.4-linux-x86_64.zip/download -o bowtie2-2.4.4.zip &&\
-    unzip bowtie2-2.4.4.zip &&\
-    mv bowtie2-2.4.4-linux-x86_64 bowtie2
+RUN python3 -m pip install -U pip &&\
+    python3 -m pip install -U matplotlib jupyterlab
 
-# Or use managed library distributions through the container OS's package
-# manager.
-RUN apt-get update -y &&\
-    apt-get install -y autoconf samtools
+# Install NUPACK
+
+## Get NUPACK via curl and unzip
+RUN curl -L https://github.com/beliveau-lab/NUPACK/archive/refs/tags/v4.0.0.23.zip -o nupack-4.0.0.23.zip &&\
+    unzip nupack-4.0.0.23.zip
+## Install with pip
+RUN python3 -m pip install -U nupack -f /root/NUPACK-4.0.0.23/src/package
 
 
-# You can use local data to construct your workflow image.  Here we copy a
-# pre-indexed reference to a path that our workflow can reference.
-COPY data /root/reference
-ENV BOWTIE2_INDEXES="reference"
+# The following lines are needed to ensure your build environement works correctly with latch.
 
-# STOP HERE:
-# The following lines are needed to ensure your build environement works
-# correctly with latch.
 COPY wf /root/wf
+RUN  sed -i 's/latch/wf/g' flytekit.config
 ARG tag
 ENV FLYTE_INTERNAL_IMAGE $tag
 RUN python3 -m pip install --upgrade latch
--- wf/__init__.py
+++ wf/__init__.py
@@ -1,99 +1,199 @@
 """
-Assemble and sort some COVID reads...
+Carry out all NUPACK's Utilities functions
 """
 
-import subprocess
+from enum import Enum
 from pathlib import Path
-
 from latch import small_task, workflow
-from latch.types import LatchFile
+from natsort import as_ascii
 
+from nupack import *  # Import NUPACK
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+from latch.types import LatchFile, LatchMetadata, LatchAuthor, LatchParameter, LatchAppearanceType, LatchRule
+from latch.resources.conditional import create_conditional_section
 
-@small_task
-def assembly_task(read1: LatchFile, read2: LatchFile) -> LatchFile:
+class Material(Enum):
+    dna = "DNA"
+    rna = "RNA"
+    rna95 = "RNA95"
 
-    # A reference to our output.
-    sam_file = Path("covid_assembly.sam").resolve()
-
-    _bowtie2_cmd = [
-        "bowtie2/bowtie2",
-        "--local",
-        "-x",
-        "wuhan",
-        "-1",
-        read1.local_path,
-        "-2",
-        read2.local_path,
-        "--very-sensitive-local",
-        "-S",
-        str(sam_file),
-    ]
-
-    subprocess.run(_bowtie2_cmd)
-
-    return LatchFile(str(sam_file), "latch:///covid_assembly.sam")
-
+class Ensemble(Enum):
+    stacking = "stacking"
+    nostacking = "nostacking"
 
 @small_task
-def sort_bam_task(sam: LatchFile) -> LatchFile:
+def nupack_analysis(
+    strands_file: LatchFile, 
+    concentrations: LatchFile,
+    material: str = Material.rna,
+    ensemble: str = Ensemble.stacking,
+    temperature: float = 37.0,
+    sodium: float = 1.0,
+    magnesium: float = 0.0,
+    out: str = "nupack-analysis",
+) -> LatchFile:
 
-    bam_file = Path("covid_sorted.bam").resolve()
+    nt_model = Model(material=material, ensemble=ensemble, celsius=temperature, sodium=sodium, magnesium=magnesium)
 
-    _samtools_sort_cmd = [
-        "samtools",
-        "sort",
-        "-o",
-        str(bam_file),
-        "-O",
-        "bam",
-        sam.local_path,
-    ]
+    with open(strands_file, "r") as f:
+        strands = f.read().splitlines()
+    f.close()
 
-    subprocess.run(_samtools_sort_cmd)
+    strand_names = []
+    strand_sequences = []
 
-    return LatchFile(str(bam_file), "latch:///covid_sorted.bam")
+    for l in strands:
+        if l.startswith('>'):
+            l = l.replace('>','').strip()
+            strand_names.append(l)
+    
+    outFile = f"/{out}.txt"
 
+    content = f"NUPACK ANALYSIS RESULTS\n{strand_names}"
+    with open(outFile, "w") as f:
+        f.write(content)
+    
+    return LatchFile(outFile, f"latch:///{outFile}")
 
-@workflow
-def assemble_and_sort(read1: LatchFile, read2: LatchFile) -> LatchFile:
-    """Description...
+metadata = LatchMetadata(
+    display_name="NUPACK Complex Analyis",
+    documentation="https://docs.nupack.org",
+    author=LatchAuthor(
+        name="NUPACK Team",
+        email="support@nupack.org",
+        github="https://github.com/beliveau-lab/NUPACK",
+    ),
+    repository="https://github.com/author/my_workflow",
+    license="BSD-3-Clause",
+)
 
-    markdown header
-    ----
+metadata.parameters["strands_file"] = LatchParameter(
+    display_name="FASTA File",
+    description="Upload FASTA file containing strands",
+    hidden=False,
+    section_title="Input",
+)
+metadata.parameters["concentrations"] = LatchParameter(
+    display_name="Concentrations File",
+    description="Upload list of concentrations",
+)
 
-    Write some documentation about your workflow in
-    markdown here:
+@workflow(metadata)
+def analysisNUPACK(
+    strands_file: LatchFile, 
+    concentrations: LatchFile,
+    material: Material = Material.rna,
+    ensemble: Ensemble = Ensemble.stacking,
+    temperature: float = 37.0,
+    sodium: float = 1.0,
+    magnesium: float = 0.0,
+    out: str = "tube-analysis",
+) -> LatchFile:
 
-    > Regular markdown constructs work as expected.
+    """NUPACK Utilities for a complex formed between two nucleotide strands
 
-    # Heading
+    # NUPACK - Utility Algorithms and Calculations
+    ---
 
-    * content1
-    * content2
+    ## **How to use**
+    ---
 
-    __metadata__:
-        display_name: Assemble and Sort FastQ Files
-        author:
-            name:
-            email:
-            github:
-        repository:
-        license:
-            id: MIT
+    1. Specify the model by choosing the nucleic acid type. This parameter is based on parameter sets obtained from different research papers. Check them out [here](https://docs.nupack.org/model/#material)
+    
+    2. Specify the sequences of the first and second strand, followed by the structure of the complex in the dot bracket notation.
 
+    3. Select the utility functions that require sequence information only.
+
+    4. Select the utility functions that require sequence and structure information.
+
+    5. Specify any other changes in the construction of the Model() object using the hidden parameters such as ensemble type and ion concentrations. 
+
+    6. Run the workflow!
+
+    ## **About**
+    ---
+
+    [NUPACK](https://docs.nupack.org/#about) is a growing software suite for the analysis and design of nucleic acid structures, devices, and systems serving the needs of researchers in the fields of nucleic acid nanotechnology, molecular programming, synthetic biology, and across the life sciences more broadly.
+
+     ## **Citations**
+    ---
+
+    ### NUPACK Analysis Algorithms
+
+    **Complex analysis and test tube analysis**
+	
+    - M.E. Fornace, N.J. Porubsky, and N.A. Pierce (2020). A unified dynamic programming framework for the analysis of interacting nucleic acid strands: enhanced models, scalability, and speed.  [ACS Synth Biol](https://pubs.acs.org/doi/abs/10.1021/acssynbio.9b00523) , 9:2665-2678, 2020. ( [pdf](http://www.nupack.org/downloads/serve_public_file/fornace20.pdf?type=pdf) ,  [supp info](http://www.nupack.org/downloads/serve_public_file/fornace20_supp.pdf?type=pdf) )
+	
+    - R. M. Dirks, J. S. Bois, J. M. Schaeffer, E. Winfree, and N. A. Pierce. Thermodynamic analysis of interacting nucleic acid strands.  [SIAM Rev](http://epubs.siam.org/doi/abs/10.1137/060651100) , 49:65-88, 2007. ( [pdf](http://www.nupack.org/downloads/serve_public_file/sirev07.pdf?type=pdf) )
+    
+    **Pseudoknot analysis**
+	
+    - R. M. Dirks and N. A. Pierce. An algorithm for computing nucleic acid base-pairing probabilities including pseudoknots.  [J Comput Chem](http://onlinelibrary.wiley.com/doi/10.1002/jcc.10296/abstract) , 25:1295-1304, 2004. ( [pdf](http://www.nupack.org/downloads/serve_public_file/jcc04.pdf?type=pdf) )
+	
+    - R. M. Dirks and N. A. Pierce. A partition function algorithm for nucleic acid secondary structure including pseudoknots.  [J Comput Chem](http://onlinelibrary.wiley.com/doi/10.1002/jcc.20057/abstract) , 24:1664-1677, 2003. ( [pdf](http://www.nupack.org/downloads/serve_public_file/jcc03.pdf?type=pdf) ,  [supp info](http://www.nupack.org/downloads/serve_public_file/jcc03_supp.pdf?type=pdf) )
+
+    **Workflow Repository** - (https://github.com/shivaramakrishna99/nupack-utility-programs)
+    
+    **Acknowledgements** - (https://docs.nupack.org/#acknowledgments)
+
+    *Authored by Shivaramakrishna Srinivasan. Feel free to reach out to me at shivaramakrishna.srinivasan@gmail.com*
+    ---
+    
     Args:
-
-        read1:
-          Paired-end read 1 file to be assembled.
-
-          __metadata__:
-            display_name: Read1
-
-        read2:
-          Paired-end read 2 file to be assembled.
-
-          __metadata__:
-            display_name: Read2
+        material:
+            __metadata__:
+                display_name: "Nucleic Acid Type"
+                _tmp:
+                    section_title: "Sequence and Structure Details"
+                appearance:
+                    comment: "Choose between DNA and RNA free energy parameter sets. Default is 'rna', based on Matthews et al., 1999"
+        ensemble:
+            __metadata__:
+                display_name: "Ensemble Type"
+                _tmp:
+                    section_title: Additional Model Specification
+                    hidden: true
+                appearance:
+                    comment: "Choose between stacking and non stacking ensemble states. Default is set to 'stacking'."
+        temperature:
+            __metadata__:
+                display_name: "Temperature (in degree Celsius)"
+                _tmp:
+                    hidden: true
+                appearance:
+                    comment: "Temperature of system. Default is 37 °C"
+        sodium:
+            __metadata__:
+                display_name: "Na+ concentration (in M)"
+                _tmp:
+                    hidden: true
+                appearance:
+                    comment: "The sum of the concentrations of (monovalent) sodium, potassium, and ammonium ions, is specified in units of molar. Default: 1.0, Range: [0.05,1.1]"
+        magnesium:
+            __metadata__:
+                display_name: "Mg++ (in nM). Default is 0 nM"
+                _tmp:
+                    hidden: true
+                appearance:
+                    comment: "The concentration of (divalent) magnesium ions, is specified in units of molar. Default: 0.0, Range: [0.0,0.2]"
+        out:
+            __metadata__:
+                display_name: "Output File Name"
+                _tmp:
+                    section_title: Output
+                appearance:
+                    comment: "Name your file containing results from chosen NUPACK Utility Programs"            
     """
-    sam = assembly_task(read1=read1, read2=read2)
-    return sort_bam_task(sam=sam)
+    
+    return nupack_analysis(
+    strands_file=strands_file,
+    concentrations=concentrations,
+    material=material,
+    ensemble=ensemble,
+    temperature=temperature,
+    sodium=sodium,
+    magnesium=magnesium,
+    out=out
+    )
